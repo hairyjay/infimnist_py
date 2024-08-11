184664ace94a6f5b5ee1bdcf9b6dc74b4949f78e
Harry J. <harryjiang0114@gmail.com>
2024-08-11 00:34:56 -0400

    Update setup.py for numpy fix

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import numpy
 
 setup(
     name = 'infimnist',
@@ -8,6 +9,7 @@ setup(
         '_infimnist',
         ['_infimnist.pyx',
          'infimnist.c',
-         'py_infimnist.c']
+         'py_infimnist.c'],
+        include_dirs=[numpy.get_include()]
         )])
 )
